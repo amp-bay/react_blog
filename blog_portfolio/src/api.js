ffd0dd00f612a64fcf1d12315ce7f40836ff155b
Amp bay <ampitanb@gmail.com>
2025-03-25 20:26:08 +0100

    Update API base URL to use environment variable

--- blog_portfolio/src/api.js
+++ blog_portfolio/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 
-export const BASE_URL='http://127.0.0.1:8000'
+export const BASE_URL=import.meta.env.VITE_BASE_URL
 
 const api=axios.create({
     baseURL: BASE_URL
